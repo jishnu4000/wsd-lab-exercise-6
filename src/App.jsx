426222528d2487a673b1aaf2c146411acbe1f213
Jishnu <jishnujayaraj455@gmail.com>
2024-08-17 23:20:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import NavBar from './components/NavBar'
 import RegistrationForm from './components/RegistrationForm'
 
 const App = () => {
   return (
     <>
-    <NavBar />
     <div className='min-h-screen bg-slate-100 flex items-center justify-center'>
       <RegistrationForm />
     </div>
