365598bf6f4795854c00d4f24304344de6b07440
Jishnu <jishnujayaraj455@gmail.com>
2024-09-02 20:07:01 +0530

    added submit handling

--- src/components/RegistrationForm.jsx
+++ src/components/RegistrationForm.jsx
@@ -117,6 +117,14 @@ const RegistrationForm = () => {
     }
   }
 
+  // form submit event handler
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (allowSubmit) {
+      alert(`User ${regName} with email ${regEmail} has been registered`)
+    }
+  }
+
   return (
     <div className="rounded-lg shadow-xl bg-white flex min-h-full flex-1 flex-col justify-center px-6 py-8 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -131,7 +139,7 @@ const RegistrationForm = () => {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form action="#" method="POST" className="space-y-6">
+        <form className="space-y-6">
           {/* Name Input */}
           <div>
             <label htmlFor="name" className="block text-sm font-medium leading-6 text-gray-900">
@@ -229,7 +237,7 @@ const RegistrationForm = () => {
           <div>
             <button
               type="submit"
-              className={`flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 ${ allowSubmit ? '' : 'opacity-50 cursor-not-allowed'}`}
+              className={`flex w-full justify-center rounded-md bg-blue-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 ${ allowSubmit ? '' : 'opacity-50 cursor-not-allowed'}`} onClick={handleSubmit}
             >
                 Register
             </button>
